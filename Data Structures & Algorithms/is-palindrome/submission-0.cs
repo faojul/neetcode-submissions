1569a2828de73a6a7a5ee52dc4dcf79e9cb6cb02
Faojul Ahsan Abir <faojulahsan@gmail.com>
2026-09-05 09:44:53 +0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if (String.IsNullOrEmpty(s))
+            return true;
+        int start = 0;
+        int end = s.Length - 1;
+
+        while (start < end) {
+            if (!char.IsLetterOrDigit(s[start])) {
+                start++;
+            } else if (!char.IsLetterOrDigit(s[end])) {
+                end--;
+            } else if (char.ToLower(s[start]) != char.ToLower(s[end]))
+                return false;
+            else {
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
